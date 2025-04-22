ad681e63275592fa79c17ff1f17749401c48b08c
Nickolaythebest <156540277+Nickolaythebest@users.noreply.github.com>
2025-04-22 13:11:05 +0200

    WIP: временные изменения user.js

---
+++ src/redux/user/operations.js
@@ -0,0 +1,36 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+
+export const apiMg = axios.create({
+    baseURL: 'http://localhost:3000/api',
+  });  
+
+export const fetchCurrentUserThunk = createAsyncThunk('user/fetchCurrentUser', async (_, thunkAPI) => {
+    try {
+        const {data} = await apiMg.get('currentUser');
+        
+        return data;
+    } catch (error) {
+        console.log('Ошибка при загрузке данных:', error.response.data);
+        return thunkAPI.rejectWithValue(error.message);
+    }
+});
+
+export const editCurrentUserThunk = createAsyncThunk(
+    'user/editCurrentUser',
+    async ({ id, name, email, balance, avatarUrl }, thunkAPI) => {
+      try {
+        const updateData = {};
+if (name !== undefined) updateData.name = name;
+if (email !== undefined) updateData.email = email;
+if (balance !== undefined) updateData.balance = balance;
+if (avatarUrl !== undefined) updateData.avatarUrl = avatarUrl;
+ // Только разрешённые поля
+  
+        const { data } = await apiMg.patch(`/user/${id}`, updateData);
+        return data;
+      } catch (error) {
+        console.log('Ошибка обновления данных:', error.response?.data);
+        return thunkAPI.rejectWithValue(error.message);
+      }
+    }
+  );
---
+++ src/redux/user/selectors.js
@@ -0,0 +1,4 @@
+
+export const selectUser = (state) => state.user.currentUser;
+export const selectIsLoading = (state) => state.user.isLoading;
+export const selectIsError = (state) => state.user.isError;
