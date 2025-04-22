d12e8e88d957e3b0d2025d1fd993f31311d3a53d
AnnnaOs <preciousanutka@gmail.com>
2025-04-22 20:22:33 +0300

    change baseURL

--- src/configAPI/api.js
+++ src/configAPI/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'https://wallet.b.goit.study/docs/',
+  baseURL: 'https://wallet-app-server-zu2t.onrender.com',
 });
 
 export const setToken = token => {
--- src/redux/auth/operations.js
+++ src/redux/auth/operations.js
@@ -1,2 +1,1 @@
-// import { createAsyncThunk } from '@reduxjs/toolkit';
-// import { api, setToken, clearToken } from '../../configAPI/api.js';
+
