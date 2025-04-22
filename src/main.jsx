cd0e4c3629511457ee51348924c6f9ce52292105
NataliiaShah <gavrilovskaboroluk@gmail.com>
2025-04-22 20:38:50 +0300

    stap 1

--- src/main.jsx
+++ src/main.jsx
@@ -2,21 +2,18 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
-import { PersistGate } from 'redux-persist/integration/react';
-
-import { store, persistor } from './redux/store';
 import App from './components/App.jsx';
 import 'modern-normalize';
 import './index.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
+    <Provider > 
+      
         <BrowserRouter>
           <App />
         </BrowserRouter>
-      </PersistGate>
+      
     </Provider>
   </StrictMode>
 );
