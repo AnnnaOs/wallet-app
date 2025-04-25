b28bf121675f3f45a95fc5a3611dc921e5ff631f
diana_dobosh <diana.lakatosh.girl@gmail.com>
2025-04-25 20:24:12 +0300

    додано розмітку

--- src/components/TransactionsItem/TransactionsItem.jsx
+++ src/components/TransactionsItem/TransactionsItem.jsx
@@ -1,14 +1,12 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-// import {
-//   deleteTransaction,
-//   fetchTransactions,
-// } from '../../redux/transactions/slice';
-import { selectAllTransactions } from '../../redux/transactions/selectors';
 import {
   deleteTransaction,
   fetchTransactions,
+  updateTransaction,
 } from '../../redux/transactions/operations';
+import { selectAllTransactions } from '../../redux/transactions/selectors';
+import { formatDate } from '../../utils/formatDate';
 
 const TransactionsItem = () => {
   const dispatch = useDispatch();
@@ -20,17 +18,40 @@ const TransactionsItem = () => {
 
   return (
     <div>
-      <h2>Transactions</h2>
-      <ul>
-        {transactions.map(t => (
-          <li key={t._id}>
-            {t.category} - {t.sum} грн
-            <button onClick={() => dispatch(deleteTransaction(t._id))}>
-              ❌
-            </button>
-          </li>
-        ))}
-      </ul>
+      <table>
+        <thead>
+          <tr>
+            <th>Date</th>
+            <th>Type</th>
+            <th>Category</th>
+            <th>Comment</th>
+            <th>Sum</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          {transactions.map(t => (
+            <tr key={t._id}>
+              <td>{formatDate(t.date)}</td>
+              <td>{t.type}</td>
+              <td>{t.category}</td>
+              <td>{t.comment}</td>
+              <td>{t.sum}</td>
+
+              <td>
+                <button onClick={() => dispatch(updateTransaction(t._id))}>
+                  ✏️
+                </button>
+              </td>
+              <td>
+                <button onClick={() => dispatch(deleteTransaction(t._id))}>
+                  Delete
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 };
--- src/pages/DashboardPage/DashboardPage.jsx
+++ src/pages/DashboardPage/DashboardPage.jsx
@@ -3,7 +3,6 @@ import TransactionsList from '../../components/TransactionsList/TransactionsList
 const DashboardPage = () => {
   return (
     <>
-      <h2>hello</h2>
       <TransactionsList />
     </>
   );
