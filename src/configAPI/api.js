9f693a6523f5a9eab01e0c2e90b111220e5182ad
Vladyslav Svintsitskyi <svintsitskyi1999@gmail.com>
2025-04-25 16:07:31 +0200

    Update api.js

--- src/configAPI/api.js
+++ src/configAPI/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'http://localhost:3000',
+baseURL: 'https://wallet-app-server-zu2t.onrender.com',
 });
 
 export const setToken = token => {
