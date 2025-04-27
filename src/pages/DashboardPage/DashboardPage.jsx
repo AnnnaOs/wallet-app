66d189c6126a39a98ac34708587e023cf822b1fc
diana_dobosh <diana.lakatosh.girl@gmail.com>
2025-04-27 09:23:12 +0300

    fix conflict from dashboard

--- src/pages/DashboardPage/DashboardPage.jsx
+++ src/pages/DashboardPage/DashboardPage.jsx
@@ -1,21 +1,24 @@
-import { NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import Header from '../../components/Header/Header.jsx';
 import HomeTab from '../HomeTab/HomeTab';
-import StatisticsTab from '../StatisticsTab/StatisticsTab.jsx';
-import s from './DashboardPage.module.css';
+import Navigation from '../../components/Navigation/Navigation.jsx';
+import { useMediaQuery } from 'react-responsive';
+import CurrencyTab from '../CurrencyTab/CurrencyTab.jsx';
+import Balance from '../../components/Balance/Balance.jsx';
 
 const DashboardPage = () => {
+  const isTablet = useMediaQuery({ minWidth: 768, maxWidth: 1279 });
+  const isDesktop = useMediaQuery({ minWidth: 1280 });
+
   return (
     <>
       <Header />
-      <div className={s.navigationWrap}>
-        <NavLink to="/statistics">
-          <StatisticsTab />
-        </NavLink>
-        <NavLink to="/">
-          <HomeTab />
-        </NavLink>
-      </div>
+      <Navigation />
+      <Balance />
+      <Link to="/home">
+        🏠 <HomeTab />
+      </Link>
+      {(isTablet || isDesktop) && <CurrencyTab />}
     </>
   );
 };
