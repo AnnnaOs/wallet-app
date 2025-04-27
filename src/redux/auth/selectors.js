3d85c1ae0e88503868c30e452e19a55599113815
NataliiaShah <gavrilovskaboroluk@gmail.com>
2025-04-27 18:22:09 +0300

    correction

--- src/redux/auth/selectors.js
+++ src/redux/auth/selectors.js
@@ -5,3 +5,5 @@ export const selectIsLoggedIn = state => state.auth.isLoggedIn;
 export const selectIsRefreshing = state => state.auth.isRefreshing;
 export const selectIsLoading = state => state.auth.isLoading;
 export const selectIsAuthError = state => state.auth.isAuthError;
+
+
